1610f9bc56f288daf52a681907713fe4fc2c8a4e
Sofianne Battikh <sofianne8@gmail.Com>
2016-11-09 13:30:28 -0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,18 @@
+<?php wp_footer(); ?>
+<!DOCTYPE HTML> 
+<html>
+<body>
+<div class="container">
+        <div class="row">
+            
+                <ul>
+        <li><a  href="http://localhost/wordpress/services/">Services</a></li>
+            <li><a  href="http://localhost/wordpress/about/">About</a></li>
+    <li><a  href="https://www.fusioncorpdesign.com/contactus">Contact</a></li>
+</ul>
+                
+            
+        </div>
+    </div>
+</body>
+</html>
